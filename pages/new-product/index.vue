457ef305bbe968cad1a3d6d24866d466e8612f15
Araby <ma7moudal3raby@gmail.com>
2024-01-30 22:32:31 +0200

    finish form

--- pages/new-product/index.vue
+++ pages/new-product/index.vue
@@ -25,7 +25,7 @@
 
       <v-autocomplete
         v-model="subCategoryID"
-        :disabled="!mainCategory"
+        :disabled="loading"
         :items="mainCategory?.children"
         color="orange"
         item-title="name"
@@ -37,26 +37,48 @@
           <v-list-item v-bind="props" :prepend-avatar="item.raw.image" :title="item.raw.name"></v-list-item>
         </template>
       </v-autocomplete>
-      <v-autocomplete
-        v-for="(field, i) in categoryFields"
-        :key="i"
-        v-model="form.properties[field.slug]"
-        :disabled="false"
-        :items="field.options"
-        :aria-describedby="field.description"
-        color="orange"
-        item-title="name"
-        item-value="id"
-        :label="field.name"
-      >
-        <template v-slot:item="{ props, item }">
-          <v-list-item v-bind="props" :title="item.raw.name"></v-list-item>
-        </template>
-      </v-autocomplete>
+      <div v-for="(field, i) in categoryFields" :key="i">
+        <v-autocomplete
+          v-model="form.properties.fields[field.slug]"
+          :disabled="loading"
+          :items="field.options"
+          :details="field.description"
+          color="razz"
+          item-title="name"
+          item-value="id"
+          :label="field.name"
+          item-children="child"
+        >
+          <template v-slot:item="{ props, item }">
+            <v-list-item v-bind="props" :title="item.raw.name"></v-list-item>
+          </template>
+          <template v-slot:append-item>
+            <v-list-item @click="selectOther(field.slug)" class="cursor-pointer" title="Other"></v-list-item>
+          </template>
+        </v-autocomplete>
+        <div class="px-3 mb-5 mt-0" v-if="form.properties.fields[field.slug] == 'other'">
+          <v-text-field
+            v-model="form.properties.others[`${field.slug}`]"
+            color="orange"
+            class="mb-2"
+            clearable
+            :label="`Other ${field.name}`"
+          ></v-text-field>
+        </div>
+      </div>
     </div>
-    <pre>
-				{{categoryFields}}
-		</pre>
+    <div class="flex justify-center">
+      <v-btn class="bg-gradient" @click="submit">Submit</v-btn>
+    </div>
+    <div class="my-10"></div>
+    <v-alert v-if="formData" type="success" variant="outlined">
+      <pre>{{ formData }}</pre>
+    </v-alert>
+    <div class="my-10"></div>
+    <div class="my-10"></div>
+    <div class="my-10"></div>
+    <div class="my-10"></div>
+    <div class="my-10"></div>
   </div>
 </template>
 
@@ -72,23 +94,41 @@ const subCategories = ref([]);
 const mainCategory = ref(null);
 const subCategoryID = ref(null);
 const categoryFields = ref(null);
+const formData = ref(null);
 const form = ref({
-  subCategoryID: null,
-  properties: {}
+  properties: {
+    menus: {},
+    fields: {},
+    others: {}
+  }
 });
 // ------------------------- Data ------------------------- //
 
 // ------------------------- Methods ------------------------- //
 function subCategorySelected() {
-  console.log("subCategoryID", subCategoryID);
   if (subCategoryID) {
     loading.value = true;
     getProperties(subCategoryID.value, res => {
       loading.value = false;
-      console.log("reeeeeeeees", res);
       categoryFields.value = res;
     });
   }
+}
+function selectOther(field) {
+  form.value.properties.fields[field] = "other";
+}
+
+function submit() {
+  let payload = {
+    mainCategoryID: mainCategory.value.id,
+    subCategoryID: subCategoryID,
+    ...form.value.properties.fields
+  };
+  Object.assign(payload, { ...form.value.properties.others });
+  formData.value = payload;
+  setTimeout(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  }, 200);
 }
 // ------------------------- Methods ------------------------- //
 
