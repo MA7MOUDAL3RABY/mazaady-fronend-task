336b6bbc2ba34762166c3ec4cf36e984bdead83e
Araby <ma7moudal3raby@gmail.com>
2024-01-30 19:26:32 +0200

    integrate properties fields

--- pages/new-product/index.vue
+++ pages/new-product/index.vue
@@ -11,7 +11,7 @@
         return-object
         label="Select Category"
         :loading="loading"
-        @update:menu="subCategory = null"
+        @update:menu="subCategoryID = null"
       >
         <template v-slot:item="{ props, item }">
           <v-list-item
@@ -24,56 +24,80 @@
       </v-autocomplete>
 
       <v-autocomplete
-        v-model="subCategory"
+        v-model="subCategoryID"
         :disabled="!mainCategory"
         :items="mainCategory?.children"
         color="orange"
         item-title="name"
         item-value="id"
         label="Select SubCategory"
+        @update:menu="subCategorySelected"
       >
         <template v-slot:item="{ props, item }">
-          <v-list-item
-            v-bind="props"
-            :prepend-avatar="item.raw.image"
-            :title="item.raw.name"
-            :subtitle="item.raw.group"
-          ></v-list-item>
+          <v-list-item v-bind="props" :prepend-avatar="item.raw.image" :title="item.raw.name"></v-list-item>
+        </template>
+      </v-autocomplete>
+      <v-autocomplete
+        v-for="(field, i) in categoryFields"
+        :key="i"
+        v-model="form.properties[field.slug]"
+        :disabled="false"
+        :items="field.options"
+        :aria-describedby="field.description"
+        color="orange"
+        item-title="name"
+        item-value="id"
+        :label="field.name"
+      >
+        <template v-slot:item="{ props, item }">
+          <v-list-item v-bind="props" :title="item.raw.name"></v-list-item>
         </template>
       </v-autocomplete>
     </div>
     <pre>
-			{{subCategory}}
+				{{categoryFields}}
 		</pre>
   </div>
 </template>
 
 <script setup>
 import { useCategoriesStore } from "~/stores/categoriesStore";
-import { ref, computed } from "vue";
-const { getAllCats } = useCategoriesStore();
+import { ref } from "vue";
+const { getAllCats, getProperties } = useCategoriesStore();
 
 // ------------------------- Data ------------------------- //
 const loading = ref();
 const categories = ref([]);
 const subCategories = ref([]);
 const mainCategory = ref(null);
-const subCategory = ref(null);
+const subCategoryID = ref(null);
+const categoryFields = ref(null);
+const form = ref({
+  subCategoryID: null,
+  properties: {}
+});
 // ------------------------- Data ------------------------- //
 
-// ------------------------- computed ------------------------- //
-// const subCategories = computed(mainCategory =>
-//   mainCategory.length ? mainCategory.children : []
-// );
-// ------------------------- computed ------------------------- //
-
 // ------------------------- Methods ------------------------- //
+function subCategorySelected() {
+  console.log("subCategoryID", subCategoryID);
+  if (subCategoryID) {
+    loading.value = true;
+    getProperties(subCategoryID.value, res => {
+      loading.value = false;
+      console.log("reeeeeeeees", res);
+      categoryFields.value = res;
+    });
+  }
+}
+// ------------------------- Methods ------------------------- //
+
+// ------------------------- mounted ------------------------- //
 getAllCats(res => {
   loading.value = false;
-  categories.value = res;
+  categories.value = res.categories;
 });
-
-// ------------------------- Methods ------------------------- //
+// ------------------------- mounted ------------------------- //
 </script>
 
 <style>
--- stores/categoriesStore.ts
+++ stores/categoriesStore.ts
@@ -7,17 +7,22 @@ export const useCategoriesStore = defineStore('categoriesStore', {
 	}),
 
 	actions: {
-		async getAllCats(callback: any) {
-
-			const response = await useFetch("https://staging.mazaady.com/api/v1/get_all_cats", {
+		async Api(url: string, callback: any) {
+			const response = await useFetch(`https://staging.mazaady.com/api/v1${url}`, {
 				method: "GET",
 				headers: {
 					"private-key": "3%o8i}_;3D4bF]G5@22r2)Et1&mLJ4?$@+16",
 				},
 				onResponse({ response }) {
-					callback(response._data.data.categories);
+					callback(response._data.data);
 				}
 			});
+		},
+		getAllCats(callback: any) {
+			this.Api("/get_all_cats", callback);
+		},
+		getProperties(CategoryID: number, callback: any) {
+			return this.Api(`/properties?cat=${CategoryID}`, callback);
 		},
 	},
 });
